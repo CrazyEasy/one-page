2989d8b5f61d72abb36da5f44351a645f9885378
EASY <linus@crazyeasy.tk>
2017-03-18 17:59:15 +0100

    r2r

--- config.php
+++ config.php
@@ -5,13 +5,13 @@
 <?php
 
 # Main domain (No "/" at the End!)
-$main_domain = "http://localhost"; 
+$main_domain = "http://example.com"; 
 
 # <title>-name
-$page_name = "OnePage";
+$page_name = "MyWebsite";
 
 # Experimental path: (case-sensitive!)
-$experimental_path = "/GitHub/one-page"; # [Leave blank, if you are using the main directory!]
+$experimental_path = ""; # [Leave blank, if you are using the main directory!]
 # Example:
 # $experimental_path = "/one-page"; [starting with a "/"!]
 
